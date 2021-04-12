e6d22f1fc9eee2a202c913cee4fb0bd9bda098ea
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-04-12 12:59:20 -0500

    pass four tests in our program

--- Calendar/Models/LeapYear.cs
+++ Calendar/Models/LeapYear.cs
@@ -4,8 +4,18 @@ namespace Calendar
   {
     public bool IsLeapYear(int year)
     {
-      // logic will go here
-      return false;      
+      if (year % 400 == 0)
+      {
+        return true;
+      }
+      else if (year % 100 == 0)
+      {
+        return false;
+      }
+      else
+      {
+        return year % 4 == 0;
+      }     
     }
   }
 }
