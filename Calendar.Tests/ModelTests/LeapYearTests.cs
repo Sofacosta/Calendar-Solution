8c51ec454272c297539801f240447f826934007d
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-04-12 12:23:53 -0500

    run the first test correctly

---
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+
+namespace Calendar.Tests
+{
+  [TestClass]
+  public class LeapYearTests
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      
+    }
+  }
+}
